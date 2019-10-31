3b7c17452856065dc075350ddb5b87f68f94f31e
Kevin Feng <fengfengkevin@gmail.com>
2019-10-30 20:39:34 -0400

    Updates

---
+++ precache-manifest.62d28c7da0682cbc53adab76d59d0e7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d62e9e2ddd1b23fac0a16aa71bb54a5",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "76b6a4884711a2df13d1",
+    "url": "/ravenous/static/css/main.39aedd63.chunk.css"
+  },
+  {
+    "revision": "279dd3558560a28a53a8",
+    "url": "/ravenous/static/js/2.dc572843.chunk.js"
+  },
+  {
+    "revision": "76b6a4884711a2df13d1",
+    "url": "/ravenous/static/js/main.ec9f7ada.chunk.js"
+  },
+  {
+    "revision": "fa22dad46b9889940621",
+    "url": "/ravenous/static/js/runtime-main.8bb26afe.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
